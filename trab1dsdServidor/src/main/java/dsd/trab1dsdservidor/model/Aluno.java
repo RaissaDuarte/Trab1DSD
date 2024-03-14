d0cfab102b3be16b9619817237d291c97722185c
RaissaDuarte <raissa.udesc.du@gmail.com>
2024-03-14 20:45:26 -0300

    14 03 ra

---
+++ trab1dsdServidor/src/main/java/dsd/trab1dsdservidor/model/Aluno.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsd.trab1dsdservidor.model;
+
+/**
+ *
+ * @author 08168578902
+ */
+public class Aluno extends Pessoa {
+    private String turma;
+
+    public Aluno(String cpf, String nome, String endereco, String turma) {
+        super(cpf, nome, endereco);
+        this.turma = turma; 
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+    
+    
+    
+    
+}

c4c118006be73c3de424ad23427204d55f1c7559
Arthur Cruz <espindola.art@gmail.com>
2024-03-14 21:09:51 -0300

    TesteArthur

---
+++ trab1dsdServidor/src/main/java/dsd/trab1dsdservidor/model/Escola.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsd.trab1dsdservidor.model;
+
+/**
+ *
+ * @author 09562388905
+ */
+public class Escola {
+    public static void main(String[] args) {
+        System.out.println("Teste Arthur");
+    }
+}
